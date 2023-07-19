dc1f91fe8cecb7bb5b4bdc5c3098275ce3eb2989
Aritra23github <aritragithub@gmail.com>
2023-07-20 02:12:13 +0530

    Episode-5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import RestaurantContainer from "./ResturantContainer";
 import restList from "../utils/mockData";
 
 const Body = () => {
+    const [listOfRestaurants, setListOfRestaurants] = useState(restList);
     return (
         <div className="body">
             <div className="search-bar">
                 Search
             </div>
+            <div className="top_rated_res">
+                <button 
+                    type="button" 
+                    className="btn btn-primary"
+                    onClick={() => {
+                        const filteredRestaurants = listOfRestaurants.filter(restaurant => restaurant.data.avgRating > 4);
+                        setListOfRestaurants(filteredRestaurants);
+                    }}
+                >
+                    Top Rated Restaurant 🍔
+                </button>
+            </div>
             <div className="res-container">
                 {
-                    restList.map(res => {
+                    listOfRestaurants.map(res => {
                         return (
                             <RestaurantContainer 
                                 key={res.data.id} 
