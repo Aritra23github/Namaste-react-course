823d67ef4b2850c7efe17c15993bafc3ef51a20f
Aritra23github <aritragithub@gmail.com>
2023-08-04 01:02:55 +0530

    episode-7

--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -11,16 +11,23 @@ export default ResturatMenu = () => {
             const response = await fetch('https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5894036&lng=88.2388162&restaurantId=427371&catalog_qa=undefined&submitAction=ENTER');
 
             const json = await response.json();
-
+            console.log(json.data.cards);
             setResInfo(json.data);
         } catch (error) {
-            console.log(error);
+            console.log('e:',error);
         }
     }
 
     return (
         <div>
-            <h1>Resturant Menu</h1>
+            <ul>    
+                <li>
+                    {resInfo?.cards[0].card.card.info.name}
+                </li>
+                <li>
+                {resInfo?.cards[0].card.card.info.locality}
+                </li>
+            </ul>
         </div>
     )
 }
