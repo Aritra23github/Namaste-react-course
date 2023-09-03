c6ca54750ace43f32d80d44c15fc9cccaaf4abcf
Aritra23github <aritragithub@gmail.com>
2023-09-04 02:13:22 +0530

    episode-12

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
 
 const appStore = configureStore({
+    // This is the reducer for our whole redux store.
     reducer: {
         cartReducer: cartSlice
     }
